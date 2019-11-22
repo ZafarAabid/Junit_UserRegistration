90a2fc69f88787a8bfc47cf131697231e8f8b717
zafaraabid <zafaraabid3295@gmail.com>
2019-11-22 18:00:48 +0530

    refactored code, seperate methods for seperate conditions

--- src/test/java/com/userRegistration/UserRegistrationTest.java
+++ src/test/java/com/userRegistration/UserRegistrationTest.java
@@ -8,14 +8,48 @@ import javax.xml.bind.ValidationEvent;
 public class UserRegistrationTest {
     ValidationOfUser validationOfuser=new ValidationOfUser();
     @Test
-        public void validateName_ifPatternMatch_returnBoolean()
+    public void validateFirstName_ifPatternMatch_AtleastOneCapitalRemainingSmallLetter_returnTrue()
     {
-        boolean result= validationOfuser.validate_FirstName("Zafar123");
-        boolean result1= validationOfuser.validate_FirstName("123456");
-        boolean result2=validationOfuser.validate_FirstName("Zafar");
-        Assert.assertFalse(result);
-        Assert.assertFalse(result1);
-        Assert.assertTrue(result2);
+        boolean result= validationOfuser.validate_FirstName("Zafar");
+        Assert.assertTrue(result);
 
     }
+    @Test
+    public void validateFirstName_ifPatternMatch_onlyCapitalLetters_returnTrue()
+    {
+        boolean result1= validationOfuser.validate_FirstName("ASDFGHJKL");
+        Assert.assertTrue(result1);
+
+    }
+    @Test
+        public void validateFirstName_combineChars_returnFalse()
+    {
+        boolean result= validationOfuser.validate_FirstName("Zafar123");
+        Assert.assertFalse(result);
+
+    }
+    @Test
+    public void validateFirstName_allNumber_returnFalse()
+    {
+        boolean result1= validationOfuser.validate_FirstName("123456");
+        Assert.assertFalse(result1);
+
+    }
+    @Test
+    public void validateFirstName_onlySmallLetters_returnFalse()
+    {
+        boolean result1= validationOfuser.validate_FirstName("asdbads");
+        Assert.assertFalse(result1);
+
+    }
+
+    @Test
+    public void validateFirstName_lessThan3char_returnFalse()
+    {
+        boolean result1= validationOfuser.validate_FirstName("As");
+        Assert.assertFalse(result1);
+
+    }
+
+    /////////////////////////////////////////////////
 }
--- src/test/java/com/userRegistration/ValidationOfUser.java
+++ src/test/java/com/userRegistration/ValidationOfUser.java
@@ -4,9 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidationOfUser {
-    private String firstName="^[a-zA-Z]{3,}";
+    private String firstName="^[A-Z][a-zA-Z]{3,}";
     public boolean validate_FirstName(String firstName) {
-        Pattern firstName_pattern = Pattern.compile("^[a-zA-Z]{3,}$");
+        Pattern firstName_pattern = Pattern.compile("^[A-Z][a-zA-Z]{3,}$");
         Matcher matcher = firstName_pattern.matcher(firstName);
         System.out.println(matcher.matches());
         if (matcher.matches())
