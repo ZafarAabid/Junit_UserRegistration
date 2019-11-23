928c67306ecd8cde4c3617d6c579a5b16ab16d9e
zafaraabid <zafaraabid3295@gmail.com>
2019-11-23 12:18:29 +0530

    functionality: adding feature to validate password Strength & requirement

---
+++ src/test/java/com/userRegistration/UserPasswordTest.java
@@ -0,0 +1,53 @@
+package com.userRegistration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserPasswordTest {
+    ValidationOfUser validationOfuser=new ValidationOfUser();
+    @Test
+    public void validatePassword_ifContainsAtleastOneUpperOneLowerOneDigitOneSpChar_returnTrue()
+    {
+        boolean result= validationOfuser.validatePassword("Xfr122@a1");
+        Assert.assertTrue(result);
+
+    }
+
+    @Test
+    public void validatePassword_iflengthIsLessThan8_returnFalse()
+    {
+        boolean result= validationOfuser.validatePassword("XFR09@m");
+        Assert.assertFalse(result);
+
+    }
+    @Test
+    public void validatePassword_ifContainsNoUppercaseLetter_returnFalse()
+    {
+        boolean result= validationOfuser.validatePassword("xfr.z@909");
+        Assert.assertFalse(result);
+
+    }
+    @Test
+    public void validatePassword_ifContainsNoLowercaseLetter_returnFalse()
+    {
+        boolean result= validationOfuser.validatePassword("XFR.Z@909");
+        Assert.assertFalse(result);
+
+    }
+    @Test
+    public void validatePassword_ifContainsNoNumeric_returnFalse()
+    {
+        boolean result= validationOfuser.validatePassword("XFR@zafar");
+        Assert.assertFalse(result);
+
+    }
+    @Test
+    public void validatePassword_ifContainsNoSpecialChar_returnFalse()
+    {
+        boolean result= validationOfuser.validatePassword("XFR123zafar");
+        Assert.assertFalse(result);
+
+    }
+
+
+}
