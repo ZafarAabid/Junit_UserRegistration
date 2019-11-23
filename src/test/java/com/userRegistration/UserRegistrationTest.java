aee7e294bbc555e6b21092fafc4c39593c51d5f4
zafaraabid <zafaraabid3295@gmail.com>
2019-11-23 09:10:58 +0530

    Hello message

--- src/test/java/com/userRegistration/UserRegistrationTest.java
+++ src/test/java/com/userRegistration/UserRegistrationTest.java
@@ -1,21 +1,2 @@
-package com.userRegistration;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import javax.xml.bind.ValidationEvent;
-
-public class UserRegistrationTest {
-    ValidationOfUser validationOfuser=new ValidationOfUser();
-    @Test
-        public void validateName_ifPatternMatch_returnBoolean()
-    {
-        boolean result= validationOfuser.validate_FirstName("Zafar123");
-        boolean result1= validationOfuser.validate_FirstName("123456");
-        boolean result2=validationOfuser.validate_FirstName("Zafar");
-        Assert.assertFalse(result);
-        Assert.assertFalse(result1);
-        Assert.assertTrue(result2);
-
-    }
-}
+//----------UserRegistrationCode--------------
+//---------------Greetings----------------
--- src/test/java/com/userRegistration/ValidationOfUser.java
+++ src/test/java/com/userRegistration/ValidationOfUser.java
@@ -1,17 +1,4 @@
-package com.userRegistration;
+//----------UserRegistrationCode--------------
+//---------------Greetings---------------- 
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class ValidationOfUser {
-    private String firstName="^[a-zA-Z]{3,}";
-    public boolean validate_FirstName(String firstName) {
-        Pattern firstName_pattern = Pattern.compile("^[a-zA-Z]{3,}$");
-        Matcher matcher = firstName_pattern.matcher(firstName);
-        System.out.println(matcher.matches());
-        if (matcher.matches())
-            return true;
-        else
-            return false;
-    }
-}
