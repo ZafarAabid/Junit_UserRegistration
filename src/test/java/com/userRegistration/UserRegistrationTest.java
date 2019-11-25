8e443db6e5e9f4103d7e7b00c247652fbfcb0fcc
zafaraabid <zafaraabid3295@gmail.com>
2019-11-25 09:27:33 +0530

    refactored code added testwise comments

--- src/test/java/com/userRegistration/UserRegistrationTest.java
+++ src/test/java/com/userRegistration/UserRegistrationTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.xml.bind.ValidationEvent;
-
 public class UserRegistrationTest {
     ValidationOfUser validationOfuser=new ValidationOfUser();
     @Test
